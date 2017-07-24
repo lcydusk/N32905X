fba4884e076895ccf82aa1f772966e0bafdd064e
sunny <lcydusk@163.com>
2017-07-24 17:26:37 +0800

    增加录音及播放mp3音频文件的功能

--- BaiduVoice-arm/sample.cpp
+++ BaiduVoice-arm/sample.cpp
@@ -9,14 +9,16 @@
 
 #define MAX_BUFFER_SIZE 512
 #define MAX_BODY_SIZE 1000000
+//#define _ACCESS_TOKEN_
 #define _VOICE_
 #define _KNOWLEDGE_
-#define _TTL_
+#define _TTS_
 
 char buffer[MAX_BUFFER_SIZE];
 
-std::string strtextvoice="";                     //
+std::string strtextvoice="";                    //
 std::string strtextknowledge = "";       	//
+std::string token="24.798d1fa7cd3aa21e7c2ad1410b084398.2592000.1503452014.282335-9863028";      //
 
 unsigned char ToHex(unsigned char x)
 {
@@ -87,14 +89,13 @@ static size_t writefunc(void *ptr, size_t size, size_t nmemb, char **result)
     }
     memcpy(*result, ptr, result_len);
     (*result)[result_len] = '\0';
-    //printf("%s\n", *result);
+    printf("%s\n", *result);
     return result_len;
 }
 
 
 int parseJsonVoice(char* input,std::string* voice)
 {
-    Json::UInt jn;
     Json::Value root;
     Json::Value temp;
     Json::Reader reader;
@@ -105,7 +106,7 @@ int parseJsonVoice(char* input,std::string* voice)
          return -1;
     }
 
-     *voice = root.get("result","")[(unsigned) 0].asString();      //voice
+     *voice = root.get("result","")[(unsigned int) 0].asString();      //voice
      //printf("result=%s\r\n",*voice .c_str());
      //["百度语音提供技术支持，"]
 }
@@ -116,6 +117,7 @@ int parseJsonKnowledge(char* input,std::string* knowledge)
 {
     Json::Value root;
     Json::Reader reader;
+    std::string str;
     bool parsingSuccessful = reader.parse(input, root);
     if(!parsingSuccessful)
     {
@@ -123,7 +125,24 @@ int parseJsonKnowledge(char* input,std::string* knowledge)
          return -1;
     }
     *knowledge = root.get("tts","").asString();      //knowledge
+    str = *knowledge;
+    
+    //str = root.get("appkey","").asString();
+    //printf("appkey:%s\r\n",str.c_str());
+    //str = root.get("operateState","").asString();
+    //printf("operateState:%s\r\n",str.c_str());
+    if(strcmp(str.c_str(),"") == 0)
+    {
+	*knowledge ="我不知道你在说什么";      //knowledge
+    }
+    else
+    {
+	*knowledge = root.get("tts","").asString();      //knowledge
+    }
+    //{"appState":{"appKey":"platform.knowledgebase","operateState":1100,"parametes":{}},"tts":"预报"  
+    
     //printf("result=%s\r\n",*knowledge .c_str());
+ 
 }
 int main (int argc,char* argv[])
 {
@@ -152,17 +171,21 @@ int main (int argc,char* argv[])
     char *apiKey = "lIpp8p5vH8RbSemWukAs5cVx";
     char *secretKey = "bx3LsPyGOMFWb6WBNzfHinxBe7qOuXK4";
 
-    std::string token;
+
     char host[MAX_BUFFER_SIZE];
     char* result = (char*)malloc(MAX_BUFFER_SIZE);
     snprintf(host, sizeof(host),
             "http://openapi.baidu.com/oauth/2.0/token?grant_type=client_credentials&client_id=%s&client_secret=%s",
             apiKey, secretKey);
+
     FILE* fpp = NULL;
     char cmd[MAX_BUFFER_SIZE];
     char* curl_cmd = "curl -s ";
     //char* curl_cmd = "wget -q -O - ";
     char* yinhao = "\"";
+
+ #ifdef _ACCESS_TOKEN_
+
     strcpy(cmd, curl_cmd);
     strcat(cmd, yinhao);
     strcat(cmd, host);
@@ -182,6 +205,8 @@ int main (int argc,char* argv[])
         }
     }
     printf("access token = %s\r\n",token.c_str());
+
+#endif
     memset(host, 0, sizeof(host));
     snprintf(host, sizeof(host), "%s", "http://vop.baidu.com/server_api");
 
@@ -283,10 +308,10 @@ int main (int argc,char* argv[])
 
     curltuling = curl_easy_init();
     curl_easy_setopt(curltuling, CURLOPT_URL, hosttuling);                       //url地址
-    curl_easy_setopt(curltuling, CURLOPT_POST, 1L);                                  //设置问非0表示本次操作为post
+    curl_easy_setopt(curltuling, CURLOPT_POST, 1L);                              //设置问非0表示本次操作为post
     curl_easy_setopt(curltuling, CURLOPT_TIMEOUT, 10);
     curl_easy_setopt(curltuling, CURLOPT_HTTPHEADER, headerlisttuling);
-    curl_easy_setopt(curltuling, CURLOPT_POSTFIELDS, bodytuling);                //post参数
+    curl_easy_setopt(curltuling, CURLOPT_POSTFIELDS, bodytuling);           //post参数
     curl_easy_setopt(curltuling, CURLOPT_WRITEFUNCTION, writefunc);         //对返回的数据进行操作的函数地址
     curl_easy_setopt(curltuling, CURLOPT_WRITEDATA, &resultBuftuling);
     restuling = curl_easy_perform(curltuling);
@@ -300,29 +325,49 @@ int main (int argc,char* argv[])
     curl_easy_cleanup(curltuling);
 
     parseJsonKnowledge(resultBuftuling,&strtextknowledge);
+ 
     printf("result=%s\r\n",strtextknowledge.c_str());
 
 #endif
-#ifdef _TTL_
-    //TTL
+#ifdef _TTS_
+    //TTS
     char* resultttl = (char*)malloc(MAX_BUFFER_SIZE);
     char hostttl[MAX_BUFFER_SIZE];
     memset(hostttl, 0, sizeof(hostttl));
     snprintf(hostttl, sizeof(hostttl), "%s", "http://tsn.baidu.com/text2audio");
 
+/*/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+tex	必填	合成的文本，使用UTF-8编码，请注意文本长度必须小于1024字节
+lan	必填	语言选择,目前只有中英文混合模式，填写固定值zh
+tok	必填	开放平台获取到的开发者access_token（见上面的“鉴权认证机制”段落）
+ctp	必填	客户端类型选择，web端填写固定值1
+cuid	必填	用户唯一标识，用来区分用户，计算UV值。建议填写能区分用户的机器 MAC 地址或 IMEI 码，长度为60字符以内
+spd	选填	语速，取值0-9，默认为5中语速
+pit	选填	音调，取值0-9，默认为5中语调
+vol	选填	音量，取值0-15，默认为5中音量
+per	选填	发音人选择, 0为普通女声，1为普通男声，3为情感合成-度逍遥，4为情感合成-度丫丫，默认为普通女声
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+*//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
     std::string lan ="zh";
     char ctp = 1;
+    char spd = 5;
+    char pit = 5;
+    char vol = 10;
+    char per = 0;
 
     std::string strtextttl  = strtextknowledge;
     strtextttl = UrlEncode(strtextttl);
 
     memset(tmp, 0, sizeof(tmp));
-    snprintf(tmp, sizeof(tmp), "?tex=%s&lan=%s&cuid=%s&ctp=%d&tok=%s", strtextttl.c_str(),lan.c_str(),cuid, ctp,token.c_str());
+    snprintf(tmp, sizeof(tmp), "?tex=%s&lan=%s&cuid=%s&ctp=%d&tok=%s&spd=%d&pit=%d&vol=%d&per=%d", strtextttl.c_str(),lan.c_str(),cuid, ctp,token.c_str(),spd,pit,vol,per);
     strcat(hostttl, tmp);
+
+	
     //printf("%s\r\n",hostttl);
 
-    //curl_cmd = "curl -s ";
-    curl_cmd = "curl -o down.wav ";
+    curl_cmd = "curl -o down.mp3 ";
     strcpy(cmd, curl_cmd);
     strcat(cmd, yinhao);
     strcat(cmd, hostttl);
